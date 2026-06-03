f448a5433aec92fa740f9e165dd0e935308eccc0
boilerhealth <boilerhealth.co@gmail.com>
2026-06-03 11:38:36 -0700

    build.js

--- build.js
+++ build.js
@@ -618,7 +618,7 @@ ${schema ? `<script type="application/ld+json">${schema}</script>` : ''}
 
 <div class="scroll-progress" id="scrollProgress"></div>
 
-<header class="site-header">
+<<header class="site-header">
   <div class="header-inner">
     <div class="logo">Boiler<span>Health</span></div>
     <nav class="main-nav">
@@ -630,7 +630,7 @@ ${schema ? `<script type="application/ld+json">${schema}</script>` : ''}
 
 ${body}
 
-<footer class="site-footer">
+<<footer class="site-footer">
   <p>© 2026 BoilerHealth. All Rights Reserved.</p>
 </footer>
 
@@ -853,7 +853,7 @@ function filterPosts(category) {
   const today = new Date().toISOString().split('T')[0];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${baseUrl}/index.html</loc>
     <lastmod>${today}</lastmod>
